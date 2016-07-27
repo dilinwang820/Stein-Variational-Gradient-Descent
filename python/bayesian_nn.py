5918eb7278ed43ec27c75ce0038e54957a8dc63d
Dilin Wang <dilin.wang.gr@dartmouth.edu>
2016-07-27 13:40:15 -0700

    Add comments

--- python/bayesian_nn.py
+++ python/bayesian_nn.py
@@ -263,6 +263,7 @@ if __name__ == '__main__':
     
     print 'Theano', theano.version.version    #our implementation is based on theano 0.8.2
                
+    np.random.seed(1)
     ''' load data file '''
     data = np.loadtxt('../data/boston_housing')
     
